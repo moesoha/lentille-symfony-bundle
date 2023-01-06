070beb5d02b76fd9f7ec3e097b2c943f288c40de
Soha Jin <soha@lohu.info>
2023-01-06 15:39:05 +0800

    routeAttr

--- src/Attribute/FrontendVisible.php
+++ src/Attribute/FrontendVisible.php
@@ -5,6 +5,7 @@ namespace Lentille\SymfonyBundle\Attribute;
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD)]
 class FrontendVisible {
 	public function __construct(
-		public readonly array $instances = []
+		public readonly array $instances = [],
+		public readonly array $attribute = []
 	) { }
 }
--- src/Frontend/ConfigEntry/RouterConfigEntry.php
+++ src/Frontend/ConfigEntry/RouterConfigEntry.php
@@ -14,33 +14,55 @@ class RouterConfigEntry implements ConfigEntryInterface {
 
 	public function getConfig(ConfigGetterArgs $args): array {
 		$routes = [];
+		$routesAttr = [];
 		/** @var Route $route */
 		foreach($this->router->getRouteCollection() as $name => $route) {
+			/** @var FrontendVisible[] $singleAttrs */
+			$singleAttrs = [];
+			/** @var FrontendVisible[] $mergedAttrs */
+			$mergedAttrs = [];
+
 			if(!is_string($controller = $route->getDefault('_controller'))) continue;
 			$controller = explode('::', $controller);
 			if(class_exists($controller[0])) {
 				$class = new \ReflectionClass($controller[0]);
-				$attributes = $class->getAttributes(FrontendVisible::class);
+				$singleAttrs = array_map(fn($a) => $a->newInstance(), $class->getAttributes(FrontendVisible::class));
+				$mergedAttrs += $singleAttrs;
 			}
 			if(isset($class) && !empty($controller[1])) {
 				try {
 					if(!empty($attrs = $class->getMethod($controller[1])->getAttributes(FrontendVisible::class))) {
-						$attributes = $attrs;
+						$singleAttrs = array_map(fn($a) => $a->newInstance(), $attrs);
+						$mergedAttrs += $singleAttrs;
 					}
 				} catch(\ReflectionException) {
 					// method may not exist
 				}
 			}
-			if($this->checkInstance($args->instance, $attributes ?? [])) {
+
+			if($this->checkInstance($args->instance, $singleAttrs)) {
 				$routes[$name] = $route->getPath();
+				$attr = array_reduce(
+					$mergedAttrs,
+					fn(array $c, FrontendVisible $a) => array_merge($c, $a->attribute),
+					[]
+				);
+				if(!empty($attr)) {
+					$routesAttr[$name] = $attr;
+				}
 			}
 		}
-		return ['route' => $routes];
+		return ['route' => $routes, 'routeAttr' => $routesAttr];
 	}
 
+	/**
+	 * @param string $instance
+	 * @param FrontendVisible[] $attributes
+	 * @return bool
+	 */
 	private function checkInstance(string $instance, array $attributes): bool {
 		foreach($attributes as $attr) {
-			$instances = $attr->getArguments()['instances'] ?? [];
+			$instances = $attr->instances ?? [];
 			if(empty($instances) || in_array($instance, $instances)) {
 				return true;
 			}
