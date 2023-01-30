2876b341b1bd0a72375b8a9e1ba46102c8c55749
Soha Jin <soha@lohu.info>
2023-01-30 14:32:09 +0800

    TranslatableHttpException

---
+++ src/Exception/TranslatableHttpException.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Lentille\SymfonyBundle\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+abstract class TranslatableHttpException extends HttpException implements TranslatableExceptionInterface {
+	private ?string $messageKey = null;
+	private array $messageData = [];
+
+	public function getMessageKey(): string {
+		return $this->messageKey ?: $this->getMessage();
+	}
+
+	public function getMessageData(): array {
+		return $this->messageData;
+	}
+
+	public function getMessageDomain(): ?string {
+		return null;
+	}
+
+	public function setTransMessage(?string $key = null, array $data = []): self {
+		$this->messageKey = $key;
+		$this->messageData = $data;
+		return $this;
+	}
+}
